3ebffccebe557ce2e0041e0152f7531308cea448
Abdouramane SIDIBE <sidibeabdouramane@hotmail.fr>
2026-06-22 14:30:42 +0200

    tp operateurs

--- src/fr/operateurs/I/E/ClasseExecutable.java
+++ src/fr/operateurs/I/E/ClasseExecutable.java
@@ -2,13 +2,19 @@ package fr.operateurs.I.E;
 
 public class ClasseExecutable {
     public static void main(String[] args) {
+        //TESTEZ LES OPERATEURS && ET ||
+
         int a = 10;
         int b = 20;
         boolean testEt = a > 0 && b < 10;
         boolean testOu = a > 0 || b < 10;
+
+        //TESTEZ LES OPERATEURS COMBINES
         a = a + 1;
         a++;
         a += 1;
+
+        //TESTEZ L’OPERATEUR TERNAIRE
         String c = a > 0 ? "a est plus grand que 0" : "a est inferieur à 0";
 
         System.out.println(a);
