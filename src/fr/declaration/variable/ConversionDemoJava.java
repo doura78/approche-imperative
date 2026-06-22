35b8393451a8c6b6be6008e9ec58363b80b80cd0
Abdouramane SIDIBE <sidibeabdouramane@hotmail.fr>
2026-06-22 14:28:53 +0200

    tp operateurs

---
+++ src/fr/declaration/variable/ConversionDemoJava.java
@@ -0,0 +1,12 @@
+package fr.declaration.variable;
+
+public class ConversionDemoJava {
+    public static void main(String[] args) {
+        byte a = 12;
+
+        double d1 = a; // conversion de byte vers double car double > byte
+        double d2 = 58.4F;
+        double l1 = d2;
+
+    }
+}
