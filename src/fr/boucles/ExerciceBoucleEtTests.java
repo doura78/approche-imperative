a8ba10b287b9e6b1b3104c1ab46e3f6ef02d26f1
Abdouramane SIDIBE <sidibeabdouramane@hotmail.fr>
2026-06-23 00:01:41 +0200

    tp tableaux, boucles et conditions

--- src/fr/boucles/ExerciceBoucleEtTests.java
+++ src/fr/boucles/ExerciceBoucleEtTests.java
@@ -4,31 +4,31 @@ public class ExerciceBoucleEtTests {
     public static void main(String[] args) {
         int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
 
-//        for (int i = 0; i < array.length; i++) {
-//            System.out.println(array[i]);
-//
-//        }
-//        for (int i = array.length - 1; i >= 0; i--) {
-//            System.out.println(array[i]);
-//        }
-//
-//        for (int i = 1; i < array.length; i++) {
-//            if (array[i] > 3) {
-//                System.out.println(array[i]);
-//            }
-//        }
-//
-//        for (int i = 0; i < array.length; i++) {
-//            if (array[i] % 2 == 0) {
-//                System.out.println(array[i]);
-//            }
-//        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
 
-//        for (int i = 0; i < array.length; i++) {
-//            if (i % 2 == 0) {
-//                System.out.println(array[i]);
-//            }
-//        }
+        }
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.println(array[i]);
+        }
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > 3) {
+                System.out.println(array[i]);
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.println(array[i]);
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                System.out.println(array[i]);
+            }
+        }
 
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 != 0) {
