c18bd825b4b5d6f5475f2efda9e14e8ad9a0da51
Abdouramane SIDIBE <sidibeabdouramane@hotmail.fr>
2026-06-22 12:07:50 +0200

    "installation projet"

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,19 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte a = -1;
+        short b = 15000;
+        int c = 1000000;
+        long d = 20000L;
+        float e = 99.99F;
+        double f = 2.5D;
+        char g = 'g';
+        boolean h = true;
+        String monString i = "Hello";
+
+
+        System.out.println(monString);
+    }
+}
+
